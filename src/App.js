09a85628dec3b8cc0809c41d39133c61040390b6
vong321654 <vongass321.32@gmail.com>
2023-12-25 11:51:54 +0700

    Hello React-red

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 function App() {
-  return (
-    <table border="1">
-      <tr><th>Product</th><th>Price</th></tr>
-      <tr><th>React</th><th>320</th></tr>
-      <tr><th>React Native</th><th>340</th></tr>
-    </table>
-  )
+  const divStyle ={
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'lager',
+    padding: '3px'
+    }
+    return <div style={divStyle}>Hello React</div>
 }
 
 export default App;
