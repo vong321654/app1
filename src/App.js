155ed4a3cba100be6c29c7886e07cba411e528cc
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-01-19 14:56:55 +0700

    RefsArray

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 //import button from './class-components';
 //import { EventData2 } from './event-data';
-import RefsFunc from  './refs-func';
+//import RefsFunc from  './refs-func';
+import RefsArray from './refs-array';
 
 function App(){
-  return <RefsFunc/>
+  return <RefsArray/>
 }
 
 export default App;
