e9a7ce3a854f1c9bdfcbf5d0a2d189a3850de4f4
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-01-19 14:18:51 +0700

    refsFunc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 //import button from './class-components';
 //import { EventData2 } from './event-data';
-import RefsFunc from  './refs-class';
+import RefsFunc from  './refs-func';
 
 function App(){
   return <RefsFunc/>
