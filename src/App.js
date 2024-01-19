47a096006c9e1a60a824a7a04c9f830fa357b555
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-01-19 15:11:36 +0700

    stateFunc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 //import button from './class-components';
 //import { EventData2 } from './event-data';
 //import RefsFunc from  './refs-func';
-import RefsArray from './refs-array';
+//import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App(){
-  return <RefsArray/>
+  return <MessageBox/>
 }
 
 export default App;
