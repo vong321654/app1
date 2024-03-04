086ddbc308b55a4428bf2ba62ff10d1bb1539722
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-03-04 11:10:45 +0700

    form_validate

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 //import Router3 from './router3';
 //import FormSearch from './form-search'
 //import FormText from './form-text'
-import FormCheck from './form-check'
+//import FormCheck from './form-check'
+import FormValidation from './form-validation'
+
 
 export default function App(){
-  return <FormCheck/>
+  return <FormValidation/>
 }
